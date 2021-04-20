1d1503a785767187db0b7828a66b3acbdd622208
Rahul Pratap Shahi (iCORE - DOP) <RA20114164@wipro.com>
2021-04-20 16:48:11 +0530

    generate JWT Token

--- api/models/users.js
+++ api/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
@@ -32,3 +33,20 @@ userSchema.methods.validPassword = function (password) {
     .toString('hex');
   return this.hash === hash;
 };
+
+
+userSchema.method.generateJwt = function () {
+  const expire = new Date();
+  expire.setDate(expire.getDate() + 7);
+  return jwt.sign({
+    _id: this._id,
+    name: this.name,
+    email: this.email,
+    exp: parseInt(expire.getTime() / 1000)
+  }, 'MY SECRET');
+  // DO NOT KEEP YOUR SECRET IN THE CODE!
+  // Note: it’s important that your secret is kept safe: 
+  // only the originating server should know what it is. 
+  // It’s best practice to set the secret as an environment variable,
+  //  and not have it in the source code, especially if your code is stored in version control somewhere.
+}
